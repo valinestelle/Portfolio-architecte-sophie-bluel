4817bce8dd8e165fd58e85b6b5310a5e411fbf87
valinestelle <125728669+valinestelle@users.noreply.github.com>
2023-05-15 17:45:44 +0200

    suppression gallery html +ajout gallery js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,8 +1,22 @@
-const gallery = document.querySelectorAll("gallery");
+const gallery = document.querySelector(".gallery");
 
 fetch("http://localhost:5678/api/works")
    .then(response => response.json())
-   .then(data => gallery.src = data[0].imageUrl)
+   .then(function (data) {
+      for (let i = 0; i < data.length; i++) {
+         let figure = document.createElement("figure");
+         let img = document.createElement("img");
+         let figcaption = document.createElement("figcaption");
+         img.src = data[i].imageUrl;
+         img.alt = data[i].title;
+         figcaption.innerHTML = data[i].title;
+         figure.appendChild(img);
+         figure.appendChild(figcaption);
+         gallery.appendChild(figure);
+      }
+   })
+
+   
    
   
 
