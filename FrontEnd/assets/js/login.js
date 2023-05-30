8ec83668162568b9ff57527ccdc17c74e356c563
valinestelle <125728669+valinestelle@users.noreply.github.com>
2023-05-30 16:05:40 +0200

    login.js modifs

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -2,27 +2,41 @@ const myForm = document.querySelector('#myForm');
 const errorMessage = document.querySelector('#message-error');
 const buttonConnect = document.querySelector('#connect-button');
 
-const login = async (email, password) => {
-  
-    const response = await fetch("http://localhost:5678/api/users/login", {
+
+const login = async (email,password) => {
+
+  const response = await fetch("http://localhost:5678/api/users/login", {
       method: "POST",
-      headers: {
+       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json",
+         Accept: "application/json",
       },
       body: JSON.stringify({
-        email: email,
+         email: email,
         password: password,
       }),
-    }).catch(function(error){
+  }).then(function(response){
+    if(response.status == 200){
+      const form = response.json();
+      const token = form.token;
+      localStorage.setItem("token", token);
+      window.location.href = "./index.html";
+      console.log(token);
+    }else{
+      errorMessage.innerHTML = "Email ou mot de passe incorrect";
+      buttonConnect.classList.add("animationbtn");
+    }})
+    .catch(function(error){
       console.log(error);
     });
-   }
+  }
 
-   buttonConnect.addEventListener('click', (e) => {
-    e.preventDefault();
-    login(myForm.email.value, myForm.password.value);
-   })
+buttonConnect.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('click');
+  login(myForm.email.value, myForm.password.value);
+});
+
 
 
 
